9cad01aa496cd1615038ad35fad0bee0a03c2da5
linhuaxin <linhuaxin@foxmail.com>
2020-04-11 00:53:49 +0800

    Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Button, { ButtonType, ButtonSize } from './components/Button/button';
+import Menu from './components/Menu/menu';
+import MenuItem from './components/Menu/menuItem';
 
 function App() {
   return (
@@ -9,6 +11,18 @@ function App() {
       <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
       <Button target="_blank" btnType={ButtonType.Link} href="http://baidu.com">Hello</Button>
       <Button disabled btnType={ButtonType.Link} href="http://baidu.com">Hello</Button>
+      <hr/>
+      <Menu onSelect={(index) => {alert(index);}}>
+        <MenuItem index={0}>
+          123
+        </MenuItem>
+        <MenuItem index={1} disabled>
+          456
+        </MenuItem>
+        <MenuItem index={2}>
+          789
+        </MenuItem>
+      </Menu>
     </div>
   );
 }
